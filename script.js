216cedecbfee81ca655f6a734ad6f9a5e14866e5
prajapat04 <124580018+prajapat04@users.noreply.github.com>
2025-05-05 12:06:57 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ console.log("JavaScript is working!");
   }
 
   window.onload = function() {
-    greetUser();
+    
 
     const newPara = document.createElement("p");
     newPara.innerHTML = "This pragraph was added user js";
@@ -40,7 +40,16 @@ console.log("JavaScript is working!");
   const name = document.getElementById("username").value.trim();
   const email = document.getElementById("email").value.trim();
   const response = document.getElementById("form-response");
-
+  const emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+ if (!emailPattern.test(email)) {
+   response.innerText = "Please enter a valid email.";
+   response.style.color = "red";
+   return;
+ } else {
+   document.getElementById("contact-form").reset();
+ 
+ };
+  
   if(name === "" || email === ""){
     response.innerText = "Please fill in all fields.";
     response.style.color = "red";
@@ -53,4 +62,36 @@ console.log("JavaScript is working!");
  document.getElementById("username").addEventListener("keyup", function(e){
   document.getElementById("form-response").innerText = `Typing : ${e.target.value}`;
  });
+
+
+ const fruits = ["apple", "banana", "cherry", "mango"];
+ const fruitList = document.getElementById("fruitList");
+ fruits.forEach(function(fruit, index){
+  console.log(`${index + 1}.${fruit}`);
+  const li = document.createElement("li");
+  li.innerText = fruit;
+  fruitList.appendChild(li);
+ });
  
+
+ const user = {
+  name : "jone",
+  age : 34,
+  email : "john@gamil.com"
+ };
+  const userInfo = document.getElementById("user-info");
+ console.log(`Name ${user.name},  Email : ${user.email}`);
+userInfo.innerText = `Name ${user.name},  Email : ${user.email}`;
+
+
+const users = [
+  {name : "Aman", age : 23},
+  {name : "Neha", age : 24},
+  {name : "Ravi", age : 26}
+];
+
+for(const user of users){
+  let li = document.createElement("li");
+  li.innerText = `${user.name} : ${user.age}`;
+  document.getElementById("obj-list").appendChild(li);
+}
